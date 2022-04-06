5585c66edaab955235385a57c80b27a93733c746
Mike Wills <mwills@bluemodus.com>
2022-04-05 20:45:20 -0700

    Added PageRelationshipInspector

---
+++ ContentReferenceModule.IntegrationTests/Inspectors/PageRelationshipInspectorTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CMS.DocumentEngine;
+using NUnit.Framework;
+using XperienceCommunity.ContentReferenceModule.Inspectors;
+using XperienceCommunity.ContentReferenceModule.Repositories;
+
+namespace ContentReferenceModule.IntegrationTests.Inspectors
+{
+    [TestFixture]
+    public class PageRelationshipInspectorTests
+    {
+        [Test()]
+        public void GetPotentialContentReferences_Returns_Discovered_Guids()
+        {
+            bool onlyPublished = false;
+            var treeNode = DocumentHelper.GetDocuments()
+                .Path("/Home")
+                .OnSite("DfwJobs.Web")
+                .Culture("en-us")
+                .WithCoupledColumns()
+                .Published(onlyPublished)
+                .LatestVersion(!onlyPublished)
+                .FirstOrDefault();
+            var pageRelationshipInspector = new PageRelationshipInspector();
+            var guidList = pageRelationshipInspector.GetPotentialContentReferences(treeNode);
+            Assert.IsNotNull(guidList);
+
+        }
+    }
+}
---
+++ ContentReferenceModule/Inspectors/PageRelationshipInspector.cs
@@ -0,0 +1,29 @@
+﻿using CMS.DocumentEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using XperienceCommunity.ContentReferenceModule.Constants;
+using XperienceCommunity.ContentReferenceModule.Core;
+
+namespace XperienceCommunity.ContentReferenceModule.Inspectors
+{
+    public class PageRelationshipInspector : IReferenceInspector
+    {
+
+        public PageRelationshipInspector()
+        {
+        }
+
+        public IEnumerable<Guid> GetPotentialContentReferences(TreeNode treeNode)
+        {
+            // TODO: Add parameter guard
+            var returnList = treeNode.RelatedDocuments
+                                     .All
+                                     .Select(n => n.NodeGUID)
+                                     .Distinct()
+                                     .ToList();
+            return returnList;
+        }
+    }
+}
